42904b97e3d997b09a0b2c2d3d4738cb32c8ff96
Gabriela Yanes <gyanes0305@gmail.com>
2021-09-07 18:59:36 -0300

    Add items to the list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
+const TODO_LIST = ["Lavar", "Pasear a Koku", "Ayudar a mi mama"];
+
+const App = () => {
+  console.log('App is being rendered')
+  const [todoList, setTodoList] = React.useState(TODO_LIST)
+
+  const addInput = () => {
+    const newTask = window.prompt("Enter your task: ");
+
+    const newTodoList = todoList.concat(newTask);
+    
+    setTodoList(newTodoList)
+  }
+
+  // const addTask = () => {
+  //   const task = 'Estudiar programacion';
+  //   const newTodoList = todoList.concat(task);
+    
+  //   setTodoList(newTodoList)
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="title">TODO LIST</h1>
+      <ul>
+        {todoList.map((todo, i) => (
+          <li key={i}>{todo}</li>
+        ))}
+      </ul>
+      <form>
+        <label>
+          Task:
+          <br />
+          <input type="text" name="name" />
+        </label>
+        <input onClick={addInput} type="submit" value="Submit" />
+      </form>
+    </>
   );
 }
 
